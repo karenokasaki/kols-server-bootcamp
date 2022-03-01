9baa7f1b1c7797c767440b611d6767a233c8d55e
LeoSantini <95887191+LeoSantini@users.noreply.github.com>
2022-03-01 16:37:00 -0300

    create middlewares auth

---
+++ middlewares/attachCurrentUser.js
@@ -0,0 +1,28 @@
+const userModel = require("../models/User.model");
+
+// Verificar se o usuário está logado
+module.exports = async (req, res, next) => {
+  try {
+    const loggedinUser = req.user;
+
+    const user = await userModel.findOne(
+      {
+        _id: loggedinUser._id,
+      },
+      // Excluindo o hash da senha e versão da resposta que vai pro servidor, por segurança
+      { passwordHash: 0, __v: 0 }
+    );
+
+    if (!user) {
+      // retorna Bad Request
+      return res.status(400).json({ msg: "User does not exist!" });
+    }
+
+    req.currentUser = user;
+
+    next();
+  } catch (error) {
+    // retorna Internal Server Error
+    return res.status(500).json({ msg: error.msg });
+  }
+};
