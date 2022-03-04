7b9e5f756a35c771b64d3871b7c0071a3c577e4d
karen okasaki <oks.karen@gmail.com>
2022-03-04 01:26:04 -0300

    fix something stupid

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        const dbConnection = await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+        const dbConnection = await mongoose.connect(process.env.MONGODB_URI)
         console.log('Conectado ao banco de dados:', dbConnection.connection.name)
     } catch (error) {
         console.log(error)
