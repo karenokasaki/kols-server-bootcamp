05b1ed734772ef117100d5a1b4524d5c97699464
LeoSantini <95887191+LeoSantini@users.noreply.github.com>
2022-03-07 17:15:56 -0300

    Insert business id log schema

--- models/Log.model.js
+++ models/Log.model.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const LogSchema = new Schema({
   userName: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  business: { type: mongoose.Schema.Types.ObjectId, ref: "Business" },
   nameProduct: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
   date: { type: Date, default: Date.now },
   quantityInput: { type: Number, min: 0 },
